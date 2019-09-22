f18875a0beabada0d9b2798e9284d4c982f12dbc
Burakhan KATDAR <burakhan.katdar@bakircay.edu.tr>
2019-09-22 13:59:57 +0300

    Proje dosyası ekle.

---
+++ Hafta-1 Toplama Ödevi/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hafta_1_Toplama_Ödevi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double Topla(double sayi1, double sayi2)
+        {
+            return (sayi1 + sayi2);
+        }
+        private void BtnTopla_Click(object sender, EventArgs e)
+        {
+            double birinciSayi, ikinciSayi, toplam;
+
+            birinciSayi = Convert.ToDouble(txtBirinciSayi.Text);
+            ikinciSayi = Convert.ToDouble(txtIkinciSayi.Text);
+
+            toplam = Topla(birinciSayi, ikinciSayi);
+
+            MessageBox.Show(Convert.ToString("Sonucunuz: " + toplam));
+
+
+
+        }
+    }
+}
